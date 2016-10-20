1a4d1ff6d5f5935f7838afc275d3b2087e1f658f
shengdexiang <shengdexiang@gmail.com>
2016-10-20 23:55:17 +0800

    add support read host from file

--- anonymous_login_ftp.py
+++ anonymous_login_ftp.py
@@ -3,7 +3,10 @@
 
 import ftptool
 import sys
+import os
 import argparse
+
+ls = os.linesep
 
 class Scanner:
     def __init__(self, host):
@@ -11,6 +14,7 @@ class Scanner:
 
     def login(self):
         try:
+            print('Testing {0}...'.format(self.host), end = ' ')
             ftp = ftptool.FTPHost.connect(self.host, user = 'anonymous', password = 'me@your.com')
             ftp.quit()
             return True
@@ -21,14 +25,30 @@ def main():
     desc = 'Host scanner on which anonymous user can login'
     parser = argparse.ArgumentParser(prog = sys.argv[0], description = desc)
     parser.add_argument('-H', dest = 'hosts', help = 'specify hosts')
+    parser.add_argument('-f', dest = 'filename', help = 'specify hosts file')
     args = parser.parse_args()
-    if args.hosts == None:
+    if args.hosts == None and args.filename == None:
         parser.print_usage()
         sys.exit(1)
-    for host in args.hosts:
-        scanner = Scanner(host)
-        if scanner.login():
-            print(host, 'FTP Anonymous Login successed.')
+    if args.hosts:
+        for host in args.hosts.split(','):
+            scanner = Scanner(host)
+            if scanner.login():
+                print(host, 'FTP Anonymous Login successed.')
+            else:
+                print('\n')
+    elif args.filename:
+        with open(args.filename, 'r') as f:
+            for host in f:
+                host = host.strip(ls)
+                scanner = Scanner(host)
+                if scanner.login():
+                    print(host, 'FTP Anonymous Login successed.')
+                else:
+                    print('\n')
+    else:
+        parser.print_usage()
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
